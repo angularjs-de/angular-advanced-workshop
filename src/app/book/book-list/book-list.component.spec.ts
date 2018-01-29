aebc94ede96b693f438058932c2464133d2e4a65
Robin Böhm <robin.boehm@workshops.de>
2018-01-29 10:43:30 +0100

    test(book-list): add simple tests for book-list

--- src/app/book/book-list/book-list.component.spec.ts
+++ src/app/book/book-list/book-list.component.spec.ts
@@ -36,4 +36,19 @@ describe('BookListComponent', () => {
     expect(component).toBeTruthy();
   });
   
+  it('should recieve 3 Books', () => {
+    expect(compiled.querySelectorAll('.book-row').length).toBe(3);
+  });
+
+  it('should display the title of each book', () => {
+    expect(compiled.querySelectorAll('.book-row')[0].innerText).toContain('Design Patterns');
+    expect(compiled.querySelectorAll('.book-row')[1].innerText).toContain('REST und HTTP');
+    expect(compiled.querySelectorAll('.book-row')[2].innerText).toContain('Eloquent JavaScript');
+  });
+
+  it('should link to the edit page of each book', () => {
+    expect(compiled.querySelectorAll('.book-row a')[0].href).toContain('/978-0-20163-361-0');
+    expect(compiled.querySelectorAll('.book-row a')[1].href).toContain('/978-3-86490-120-1');
+    expect(compiled.querySelectorAll('.book-row a')[2].href).toContain('/978-1-59327-584-6');
+  });
 });
