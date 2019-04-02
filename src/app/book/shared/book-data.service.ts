07dbe773d83eb58fdf56c86ba990a3fb0682bac6
Dave <dave@webdave.de>
2019-04-02 17:40:14 +0200

    V7

--- src/app/book/shared/book-data.service.ts
+++ src/app/book/shared/book-data.service.ts
@@ -1,29 +1,25 @@
-import { Injectable } from '@angular/core';
-import { Book } from './book';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-
+import { Injectable } from "@angular/core";
+import { Book } from "./book";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class BookDataService {
-
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   getBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>('http://localhost:4730/books')
+    return this.http.get<Book[]>("http://localhost:4730/books");
   }
 
   getBookByIsbn(isbn: string): Observable<Book> {
-    return this.http.get<Book>(`http://localhost:4730/books/${isbn}`)
+    return this.http.get<Book>(`http://localhost:4730/books/${isbn}`);
   }
 
   createBook(book: Book): Observable<Book> {
-    return this.http.post<Book>('http://localhost:4730/books', book)
+    return this.http.post<Book>("http://localhost:4730/books", book);
   }
 
   updateBook(isbn: string, vector: any): Observable<Book> {
-    return this.http.patch<Book>(`http://localhost:4730/books/${isbn}`, vector)
+    return this.http.patch<Book>(`http://localhost:4730/books/${isbn}`, vector);
   }
-
 }
