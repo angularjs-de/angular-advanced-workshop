c035b68c1a7b8820ba272334612978b4af7e9ffe
Dave <dave@webdave.de>
2019-04-09 10:44:59 +0200

    BookStaticDataService

--- src/app/book/shared/book-static-data.service.spec.ts
+++ src/app/book/shared/book-static-data.service.spec.ts
@@ -1,67 +1,74 @@
+// tslint:disable:max-line-length
 import { BookStaticDataService } from './book-static-data.service';
 import { Book } from './book';
 
-describe('BookStaticDataService', () => {
+let service: BookStaticDataService;
 
+beforeEach(() => {
+  service = new BookStaticDataService();
+});
+fdescribe('BookStaticDataService', () => {
   it('should be created', () => {
-    //expect(service).toBeTruthy();
-    expect(true).toBeFalsy();
+    expect(service).toBeTruthy();
   });
 
   describe('getBooks()', () => {
     it('should return the complete array of static data', () => {
-      expect(true).toBeFalsy();
+      expect(service.getBooks()).toBe(service.staticBookData);
     });
   });
 
   describe('getBook(isbn)', () => {
     it('should always return the first element of data', () => {
-      expect(true).toBeFalsy();
+      expect(service.getBook('123').isbn).toBe(service.staticBookData[0].isbn);
     });
   });
 
-  describe('getBook(book)', () => {
+  describe('updateBook(book)', () => {
     it('should return the given book argument itself', () => {
-      expect(true).toBeFalsy();
+      const book = service.staticBookData[1];
+      book.title = 'Moin';
+      expect(service.updateBook(book).title).toBe('Moin');
     });
   });
-
 
   describe('createBook(book)', () => {
     it('should return the book argument itself', () => {
-
-      // const book: Book = {
-      //   'title': 'The New Design Patterns',
-      //   'subtitle': 'Elements of Reusable Object-Oriented Software',
-      //   'isbn': '978-0-20163-361-0',
-      //   'abstract': 'Capturing a wealth of experience about the design of object-oriented software, four top-notch designers present a catalog of simple and succinct solutions to commonly occurring design problems. Previously undocumented, these 23 patterns allow designers to create more flexible, elegant, and ultimately reusable designs without having to rediscover the design solutions themselves.',
-      //   'numPages': 395,
-      //   'author': 'Erich Gamma / Richard Helm / Ralph E. Johnson / John Vlissides',
-      //   'publisher': {
-      //     'name': 'Addison-Wesley',
-      //     'url': 'http://www.addison-wesley.de/'
-      //   }
-      // };
-      expect(true).toBeFalsy();
-
+      const book: Book = {
+        title: 'The New Design Patterns',
+        subtitle: 'Elements of Reusable Object-Oriented Software',
+        isbn: '978-0-20163-361-0',
+        abstract:
+          'Capturing a wealth of experience about the design of object-oriented software, four top-notch designers present a catalog of simple and succinct solutions to commonly occurring design problems. Previously undocumented, these 23 patterns allow designers to create more flexible, elegant, and ultimately reusable designs without having to rediscover the design solutions themselves.',
+        numPages: 395,
+        author:
+          'Erich Gamma / Richard Helm / Ralph E. Johnson / John Vlissides',
+        publisher: {
+          name: 'Addison-Wesley',
+          url: 'http://www.addison-wesley.de/'
+        }
+      };
+      expect(service.createBook(book)).toBe(book);
     });
 
     it('should add the new book to the dataset', () => {
-      // const book: Book = {
-      //   'title': 'The New Design Patterns',
-      //   'subtitle': 'Elements of Reusable Object-Oriented Software',
-      //   'isbn': '978-0-20163-361-0',
-      //   'abstract': 'Capturing a wealth of experience about the design of object-oriented software, four top-notch designers present a catalog of simple and succinct solutions to commonly occurring design problems. Previously undocumented, these 23 patterns allow designers to create more flexible, elegant, and ultimately reusable designs without having to rediscover the design solutions themselves.',
-      //   'numPages': 395,
-      //   'author': 'Erich Gamma / Richard Helm / Ralph E. Johnson / John Vlissides',
-      //   'publisher': {
-      //     'name': 'Addison-Wesley',
-      //     'url': 'http://www.addison-wesley.de/'
-      //   }
-      // };
-      expect(true).toBeFalsy();
-
+      const book: Book = {
+        title: 'The New Design Patterns',
+        subtitle: 'Elements of Reusable Object-Oriented Software',
+        isbn: '978-0-20163-361-0',
+        abstract:
+          'Capturing a wealth of experience about the design of object-oriented software, four top-notch designers present a catalog of simple and succinct solutions to commonly occurring design problems. Previously undocumented, these 23 patterns allow designers to create more flexible, elegant, and ultimately reusable designs without having to rediscover the design solutions themselves.',
+        numPages: 395,
+        author:
+          'Erich Gamma / Richard Helm / Ralph E. Johnson / John Vlissides',
+        publisher: {
+          name: 'Addison-Wesley',
+          url: 'http://www.addison-wesley.de/'
+        }
+      };
+      expect(service.getBooks().length).toBe(3);
+      service.createBook(book);
+      expect(service.getBooks().length).toBe(4);
     });
   });
-
 });
