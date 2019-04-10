c6fb5977bdd2138498331e581668fde28ba2a49a
Dave <dave@webdave.de>
2019-04-10 15:22:59 +0200

    fix V7

--- src/app/book/store/books.effects.ts
+++ src/app/book/store/books.effects.ts
@@ -1,21 +1,18 @@
-import { Injectable } from '@angular/core';
-import { Effect, Actions } from '@ngrx/effects';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { Effect, Actions, ofType } from "@ngrx/effects";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import { Action } from "@ngrx/store";
-import { LOAD_BOOKS, ReadyBooks } from './books.actions';
-
+import { LOAD_BOOKS, ReadyBooks } from "./books.actions";
 
 @Injectable()
 export class BookEffects {
- // Listen for the LOAD_BOOKS action
- @Effect()
- login$: Observable<Action> = this.actions$
-   .ofType(LOAD_BOOKS)
-   .pipe(
-       map(action => new ReadyBooks())
-    );
+  // Listen for the LOAD_BOOKS action
+  @Effect()
+  login$: Observable<Action> = this.actions$.pipe(
+    ofType(LOAD_BOOKS),
+    map(action => new ReadyBooks())
+  );
 
- constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions) {}
 }
-
--- src/app/book/store/books.reducer.ts
+++ src/app/book/store/books.reducer.ts
@@ -1,46 +1,48 @@
 import { Action } from "@ngrx/store";
-import { Book } from '../shared/book';
+import { Book } from "../shared/book";
 import * as BooksActions from "./books.actions";
-import { ReadyBooks } from './books.actions';
+import { ReadyBooks } from "./books.actions";
 
 export const booksStoreName = "booksStore";
 
 export interface BooksState {
-    books: Book[];
+  books: Book[];
 }
 
 export const initialState: BooksState = {
-    books: [{
-        'title': 'The New Design Patterns',
-        'subtitle': 'Elements of Reusable Object-Oriented Software',
-        'isbn': '978-0-20163-361-0',
-        'abstract': 'Capturing a wealth of experience about the design of object-oriented software, four top-notch designers present a catalog of simple and succinct solutions to commonly occurring design problems. Previously undocumented, these 23 patterns allow designers to create more flexible, elegant, and ultimately reusable designs without having to rediscover the design solutions themselves.',
-        'numPages': 395,
-        'author': 'Erich Gamma / Richard Helm / Ralph E. Johnson / John Vlissides',
-        'publisher': {
-          'name': 'Addison-Wesley',
-          'url': 'http://www.addison-wesley.de/'
-        }
-      }]
+  books: [
+    {
+      title: "The New Design Patterns",
+      subtitle: "Elements of Reusable Object-Oriented Software",
+      isbn: "978-0-20163-361-0",
+      abstract:
+        "Capturing a wealth of experience about the design of object-oriented software, four top-notch designers present a catalog of simple and succinct solutions to commonly occurring design problems. Previously undocumented, these 23 patterns allow designers to create more flexible, elegant, and ultimately reusable designs without having to rediscover the design solutions themselves.",
+      numPages: 395,
+      author: "Erich Gamma / Richard Helm / Ralph E. Johnson / John Vlissides",
+      publisher: {
+        name: "Addison-Wesley",
+        url: "http://www.addison-wesley.de/"
+      }
+    }
+  ]
 };
 
-
 export function booksReducer(state = initialState, action: Action): BooksState {
-    switch (action.type) {
-        case BooksActions.LOAD_BOOKS: {
-            const LOAD_BOOKS = <BooksActions.LoadBooks>action;
-            return {
-                ...state,
-                books: LOAD_BOOKS.books
-            };
-        }
-        case BooksActions.READY_BOOKS: {
-            const READY_BOOKS = <BooksActions.ReadyBooks>action;
-            console.log("Books Loaded");
-            return  {...state };
-        }
-        default: {
-            return state;
-        }
+  switch (action.type) {
+    case BooksActions.LOAD_BOOKS: {
+      const LOAD_BOOKS = <BooksActions.LoadBooks>action;
+      return {
+        ...state,
+        books: LOAD_BOOKS.books
+      };
     }
+    case BooksActions.READY_BOOKS: {
+      const READY_BOOKS = <BooksActions.ReadyBooks>action;
+      console.log("Books Loaded");
+      return { ...state };
+    }
+    default: {
+      return state;
+    }
+  }
 }
