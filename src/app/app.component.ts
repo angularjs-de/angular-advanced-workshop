6744afa8e25cffb39f2862ce5d33582ba6467121
Robin Böhm <robin.boehm@workshops.de>
2019-09-12 15:21:00 +0200

    feat(i18n): extract all translatable strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,4 @@ import { TranslateService } from '@ngx-translate/core';
   selector: 'ws-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'de']);
-    translate.setDefaultLang('en');
-
-    const browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/en|de/) ? browserLang : 'en');
-  }
-}
+export class AppComponent {}
